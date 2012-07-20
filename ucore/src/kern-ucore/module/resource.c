c30a49857e7e02984f0b46eeabe9f9def17fd1e0
Chen Yuheng <chyh1990@gmail.com>
2012-07-20 20:41:01 +0800

    add file

---
+++ ucore/src/kern-ucore/module/resource.c
@@ -0,0 +1,55 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  resource.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  07/20/2012 08:32:48 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Chen Yuheng (Chen Yuheng), chyh1990@163.com
+ *   Organization:  Tsinghua Unv.
+ *
+ * =====================================================================================
+ */
+#include <linux/module.h>
+#include <linux/errno.h>
+#include <linux/ioport.h>
+#include <linux/init.h>
+#include <linux/slab.h>
+#include <linux/spinlock.h>
+#include <linux/fs.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/device.h>
+#include <linux/pfn.h>
+#include <asm/io.h>
+
+/* resource */
+struct resource ioport_resource = {
+  .name   = "PCI IO",
+  .start  = 0,
+  .end    = IO_SPACE_LIMIT,
+  .flags  = IORESOURCE_IO,
+};
+
+struct resource iomem_resource = {
+  .name   = "PCI mem",
+  .start  = 0,
+  .end    = -1,
+  .flags  = IORESOURCE_MEM,
+};
+
+int insert_resource(struct resource *parent, struct resource *new)
+{
+  return 0;
+}
+
+int release_resource(struct resource *old)
+{
+  return 0;
+}
+
